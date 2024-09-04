07c91aa60ac03d5a1d5a0d42d57475989f62ee0e
Thales Moraes <thales.moraes04@outlook.com>
2024-09-04 15:44:29 -0300

    add js

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        title : "Rayssa Leal",
+        description : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        title : "Rebeca Andrade",
+        description : "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        title : "Beatriz Souza",
+        description : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
