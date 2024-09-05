b27193a23f4fe25aedb7c1b9ae170c0666caa2c7
Thales Moraes <thales.moraes04@outlook.com>
2024-09-05 14:43:21 -0300

    add new data

--- dados.js
+++ dados.js
@@ -1,17 +1,22 @@
 let dados = [
     {
-        title : "Rayssa Leal",
-        description : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        title : "Let Go",
+        description : "'Let Go' é uma das faixas de maior sucesso do rapper. A música, marcada por um ritmo envolvente e letras sinceras, aborda temas como relacionamentos, ciúmes e a dificuldade de seguir em frente após um término.",
+        link : "https://open.spotify.com/intl-pt/track/3zkyus0njMCL6phZmNNEeN?si=40377a2e3087449a"
     },
     {
-        title : "Rebeca Andrade",
-        description : "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
-        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        title : "Obsessed With You",
+        description : "'Obsessed With You' é uma colaboração entre o rapper britânico e a cantora PinkPantheress. A música, lançada em 2022, rapidamente se tornou um sucesso viral, conquistando milhões de visualizações e ouvintes em todo o mundo. A letra de 'Obsessed With You' aborda temas como amor, ciúmes e a complexidade dos relacionamentos modernos.",
+        link : "https://open.spotify.com/intl-pt/track/4oxGI8yyghLuMV7ybRG3kg?si=d194b01d0b694bd2"
     },
     {
-        title : "Beatriz Souza",
-        description : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
-        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        title : "Sprinter",
+        description : "'Sprinter' é mais uma joia da coroa do rapper britânico, que continua a solidificar sua posição como um dos nomes mais relevantes do cenário musical atual. A letra de 'Sprinter' é uma ode à vida rápida, ao sucesso e à ambição. Central Cee se compara a um velocista, um 'sprinter', que está determinado a alcançar seus objetivos a qualquer custo.",
+        link : "https://open.spotify.com/intl-pt/track/2FDTHlrBguDzQkp7PVj16Q?si=6145c3233cae403a"
+    },
+    {
+        title : "Day in The Life",
+        description : "'Day in the Life' é uma faixa do rapper que oferece um vislumbre de seu cotidiano e das realidades da vida nas ruas. A letra de 'Day in the Life' retrata um dia típico na vida de Central Cee, envolvendo atividades relacionadas à sua vida de gangue, como tráfico de drogas, brigas e confrontos com rivais. O rapper descreve a violência, a tensão e os perigos que cercam seu ambiente.",
+        link : "https://open.spotify.com/intl-pt/track/1o6x7iRcDQPEEg8YPgLeVA?si=2c44003e6f0d41da"
     }
 ];
