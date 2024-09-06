5e2dce32f2c256cd9ea90a9a92f88a9c38d02bb0
Thales Moraes <thales.moraes04@outlook.com>
2024-09-05 22:17:43 -0300

    js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(dados)
+// console.log(dados)
+
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+section.innerHTML = `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">Let Go</a>
+        </h2>
+        <p class="descricao-meta">'Let Go' é uma das faixas de maior sucesso do rapper. A música, marcada por um ritmo envolvente e letras sinceras, aborda temas como relacionamentos, ciúmes e a dificuldade de seguir em frente após um término.</p>
+        <a href="https://www.youtube.com/watch?v=BqucGvLxrEk" target="_blank">Ouça aqui</a>
+    </div>
+`
