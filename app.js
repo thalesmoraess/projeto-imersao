7c5ae50be8f29da043aafee8fbec49289721a457
Thales Moraes <thales.moraes04@outlook.com>
2024-09-07 13:22:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa")
-    console.log(section);
-
-    let resultados = ""
-
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  
+    // Itera sobre os dados da pesquisa e cria HTML para cada resultado
     for (let dado of dados) {
-    resultados += `
-    <div class="item-resultado">
-        <h2>
+      // Concatena o HTML de um item de resultado à string de resultados
+      resultados += `
+        <div class="item-resultado">
+          <h2>
             <a href="#" target="_blank">${dado.title}</a>
-        </h2>
-        <p class="descricao-meta">${dado.description}</p>
-        <a href="${dado.link}" target="_blank">Ouça aqui</a>
-    </div>
-`
-}
-
-section.innerHTML = resultados
-}
+          </h2>
+          <p class="descricao-meta">${dado.description}</p>
+          <a href="${dado.link}" target="_blank">Ouça aqui</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
